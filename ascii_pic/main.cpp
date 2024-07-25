7c94b7167008f826c246b3533580e24a198da201
grigoriok <grigoriok@it.auth.gr>
2024-07-25 19:48:41 +0300

    add build script, main and todo list

---
+++ ascii_pic/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+// load image
+// create ASCII array
+// transform image to greyscale with floating point values from 0.0 to 1.0
+// based on the floating point value, replace the pixel with an ASCII character that matches the luminosity density
+
+int main() {
+    std::vector<char> asciiChars = {
+        ' ', '.', '`', ',', ':', ';', 'i', 'l', '!', '^', '+', '*', '?', 
+        '%', 'S', '#', '@', 'M', 'W', '&', '8', 'B', '$'
+    };
+
+    return 0;
+}
